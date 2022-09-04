93ee8d9f2cbe2cd7d528cc6b0c02d1f744238247
akiev33 <71421550+akiev33@users.noreply.github.com>
2022-09-04 21:31:34 +0600

    new commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_lesson_21/text_field.dart';
+import 'dart:math';
 
 void main() {
   runApp(const MyApp());
@@ -38,7 +38,14 @@ class _HomeScreenState extends State<HomeScreen> {
   final emailFocus = FocusNode();
   final passwordFocus = FocusNode();
 
+  List<String> names = ['Eldar', 'Taalai', 'Erjan'];
+  String nameValue = 'Eldar';
+  bool isCheckActive = false;
+  bool isSwitchActive = false;
+  final random = Random();
   //
+  Color _color = Colors.green;
+  BorderRadiusGeometry _borderRadius = BorderRadius.circular(9);
 
   @override
   void initState() {
@@ -79,6 +86,12 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey.shade300,
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: () {},
+        backgroundColor: Colors.black,
+        label: const Text('search'),
+        icon: const Icon(Icons.search),
+      ),
       body: SafeArea(
         child: SingleChildScrollView(
           child: Column(
@@ -121,45 +134,129 @@ class _HomeScreenState extends State<HomeScreen> {
                           width: double.infinity,
                           height: 50,
                           child: ElevatedButton(
+                            // onLongPress: () {
+                            //   print('on long press');
+                            // },
+                            // onHover: (value) {
+                            //   print(value);
+                            // },
                             onPressed: isEmailFilled && isPasswordFilled
                                 ? () {
                                     formKey.currentState?.validate();
                                     authUser();
                                   }
                                 : null,
-                            child: const Text('Join'),
+                            style: ElevatedButton.styleFrom(
+                              elevation: 10,
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                              primary: Colors.black,
+                            ),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: const [
+                                Icon(Icons.alarm),
+                                SizedBox(width: 12),
+                                Text('Join'),
+                              ],
+                            ),
                           ),
                         ),
                         const SizedBox(height: 50),
                         Center(child: Text(isAuth)),
+                        TextButton(
+                          onPressed: () {
+                            print('its text button');
+                          },
+                          child: const Text('Registration'),
+                        )
                       ],
                     ),
                   ),
                 ),
               ),
               const SizedBox(height: 50),
-              Center(
-                child: RichText(
-                  text: const TextSpan(
-                    style: TextStyle(
-                      color: Colors.black,
-                      fontSize: 16,
-                      fontWeight: FontWeight.w500,
+              DropdownButton<String>(
+                value: nameValue,
+                items: [
+                  ...names.map((e) {
+                    return DropdownMenuItem(
+                      value: e,
+                      child: Text(e),
+                    );
+                  }).toList(),
+                ],
+                onChanged: (name) {
+                  nameValue = name ?? '';
+                  email.text = name ?? '';
+                  setState(() {});
+                },
+              ),
+              Checkbox(
+                value: isCheckActive,
+                onChanged: (value) {
+                  isCheckActive = !isCheckActive;
+                  setState(() {});
+                },
+              ),
+
+              Switch.adaptive(
+                  value: isSwitchActive,
+                  onChanged: (value) {
+                    isSwitchActive = value;
+                    setState(() {});
+                  }),
+              // Center(
+              //   child: RichText(
+              //     text: const TextSpan(
+              //       style: TextStyle(
+              //         color: Colors.black,
+              //         fontSize: 16,
+              //         fontWeight: FontWeight.w500,
+              //       ),
+              //       children: [
+              //         TextSpan(text: 'Dont have an account?'),
+              //         TextSpan(
+              //           text: ' Sign up',
+              //           style: TextStyle(
+              //             color: Colors.blue,
+              //             fontSize: 16,
+              //             fontWeight: FontWeight.w500,
+              //           ),
+              //         ),
+              //       ],
+              //     ),
+              //   ),
+              // ),
+              if (isSwitchActive)
+                Center(
+                  child: AnimatedContainer(
+                    height: random.nextDouble() * 250,
+                    width: random.nextDouble() * 250,
+                    decoration: BoxDecoration(
+                      color: Color.fromRGBO(random.nextInt(500),
+                          random.nextInt(500), random.nextInt(500), 1),
+                      borderRadius: _borderRadius,
                     ),
-                    children: [
-                      TextSpan(text: 'Dont have an account?'),
-                      TextSpan(
-                        text: ' Sign up',
-                        style: TextStyle(
-                          color: Colors.blue,
-                          fontSize: 16,
-                          fontWeight: FontWeight.w500,
-                        ),
-                      ),
-                    ],
+                    duration: const Duration(seconds: 1),
+                    curve: Curves.fastOutSlowIn,
                   ),
                 ),
-              ),
+              if (!isSwitchActive)
+                Center(
+                  child: AnimatedContainer(
+                    height: random.nextDouble() * 250,
+                    width: random.nextDouble() * 250,
+                    decoration: BoxDecoration(
+                      color: Color.fromRGBO(random.nextInt(500),
+                          random.nextInt(500), random.nextInt(500), 1),
+                      borderRadius: _borderRadius,
+                    ),
+                    duration: const Duration(seconds: 1),
+                    curve: Curves.fastOutSlowIn,
+                  ),
+                ),
             ],
           ),
         ),
